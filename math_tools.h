70a6a1aa51fe6924ca3ac4bf87a714d53454033c
Dragos <dragoshuiu12@gmail.com>
2025-12-26 22:32:42 +0200

    Add the signatures of the functions

--- math_tools.h
+++ math_tools.h
@@ -1,1 +1,19 @@
 #include "math_tools.c"
+
+// Here you have the signutaures of the functions
+
+// Trigonometric functions
+long double sin(long double arg);
+long double cos(long double arg);
+long double tan(long double arg);
+long double cot(long double arg);
+
+// Inverse trigonometric functions
+long double arcsin(long double arg);
+long double arccos(long double arg);
+long double arctan(long double arg);
+long double arccot(long double arg);
+
+// Other functions
+long double power(long double n, long double power);
+long double sqrt(long double n);
