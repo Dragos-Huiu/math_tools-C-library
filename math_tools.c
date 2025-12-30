41c9f7f1b546ccb9d126e3232b135e2069602de4
Dragos <dragoshuiu12@gmail.com>
2025-12-30 15:13:59 +0200

    Implement trigonometric functions

--- math_tools.c
+++ math_tools.c
@@ -7,7 +7,7 @@ long double sin(long double arg) {
 	// Move the arg value in [0, PI/2] for faster convergence
 	int change = 1; // Change the final result sign if we move by PI
 	if (arg < 0) {
-		arg += 2*PI;
+		arg += TWO_PI;
 	}
 	if (arg > 3 * PI / 2) {
 		// quadran 4
@@ -36,11 +36,110 @@ long double sin(long double arg) {
 	return result * change;
 }
 
+long double cos(long double arg) {
+	arg -= TWO_PI * (long long)(arg / TWO_PI);
+	// Move the arg value in [0, PI/2] for faster convergence
+	if (arg < 0) {
+		arg += TWO_PI;
+	}
+	int change = 1;
+	if (arg > 3 * PI / 2) {
+		// quadran 4
+		arg = TWO_PI - arg;
+	} else if (arg > PI) {
+		// quadran 3
+		change = -change;
+		arg -= PI;
+		if (arg > PI / 2) {
+			arg = PI - arg;
+		}
+	} else if (arg > PI / 2) {
+		change = -change;
+		arg = PI - arg;
+	}
+	// Use Taylor Series
+	// cos(x) = Σ[(-1)^n * x^(2n) / (2n)!], with n from 0 to infinity
+	long double term = 1;
+	long double result = 1;
+	for (int i = 1; term <= -EPS || term >= EPS; i += 2) {
+		term *= -1.0 * (arg * arg) / (i * (i + 1));
+		result += term;
+	}
+	return result * change;
+}
+
+long double tan(long double arg) {
+	return sin(arg) / cos(arg);
+}
+
+long double cot(long double arg) {
+	return cos(arg) / sin(arg);
+}
+
+long double arcsin(long double arg) {
+	/* 
+		Use Newton-Raphson method
+		xk = xk-1 - f(xk-1) / f'(xk-1), with k from 0 to infinity
+		The function: f(x) = sin(x) - arg
+		The derivative: f'(x) = cos(x)
+		from x = arcsin(arg), simplified sin(x) = arg => sin(x) - arg = 0
+	*/
+	if (arg < -1 || arg > 1) {
+		return __INT_MAX__;
+	}
+	long double current_x = arg; // start with a x prediction
+	while (1) {
+		// Simplify the ecuation to get the new x
+		long double new_x = current_x - tan(current_x) + (arg / cos(current_x));
+		if (new_x - current_x >= -EPS && new_x - current_x <= EPS) {
+			/*
+				return the new x if the difference between the new x and the
+				current x is in [-EPS, EPS]
+			*/
+			return new_x;
+		}
+		current_x = new_x; // Continue the process for new value approximation
+	}
+}
+
+long double arccos(long double arg) {
+	return PI/2 - arcsin(arg);
+}
+
+long double arctan(long double arg) {
+	/* 
+		Use Newton-Raphson method
+		xk = xk-1 - f(xk-1) / f'(xk-1), with k from 0 to infinity
+		The function: f(x) = x^2 - n
+		The derivative: f'(x) = 2*x
+		from x = arctan(n), simplified tan(x) = n => tan(x) - n = 0
+	*/
+	long double current_x = arg; // Start with the prediction set to arg
+	while (1) {
+		// Simplify the ecuation to get the new x
+		long double new_x = current_x - (cos(current_x) * cos(current_x)) *
+							(tan(current_x) - arg);
+		if (new_x - current_x > -EPS && new_x - current_x < EPS) {
+			/*
+			The loop stops by returning the new x when the absolute value
+			of the difference between new x and current x is less than epsilon
+			*/
+			return new_x;
+		}
+		current_x = new_x; // Continue the process for new value approximation
+	}
+}
+
+long double arccot(long double arg) {
+	return PI/2 - arctan(arg);
+}
+
 long double sqrt(long double n) {
 	/* 
 		Use Newton-Raphson method
 		xk = xk-1 - f(xk-1) / f'(xk-1), with k from 0 to infinity
 		The function: f(x) = x^2 - n
+		The derivative: f'(x) = 2*x
 		from x = sqrt(n), simplified x^2 = n => x^2 - n = 0
 	*/
 	long double current_x = n / 2.0; // Start with the prediction set to n / 2.0
